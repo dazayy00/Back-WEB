b37a9ec9752478ccf7326e9ba1ec296ace44578e
dazayy <zs21020562@estudiantes.uv.mx>
2024-06-09 22:54:09 -0600

    disp

--- PetPlusApi/home.py
+++ PetPlusApi/home.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI, Depends, HTTPException
-from database import get_admin_by_matricula
+from fastapi import FastAPI, HTTPException
 from models import Admin
+from bson.objectid import ObjectId  
+from database import get_one_admin_by_id
 
 app = FastAPI()
 
+#imprimir datos de admin
+@app.get("/api/admins/{id}", response_model=Admin)
+async def get_admin_by_id(id: str):
+    admin = await get_one_admin_by_id(id)
+    if admin:
+        return admin
+    else:
+        raise HTTPException(status_code=404, detail=f"Admin with ID '{id}' not found")
+    
 
-@app.get("/admin")
-async def get_admin_data(matricula: str):
-    db_admin = await get_admin_by_matricula(matricula)
-
-    if not db_admin:
-        raise HTTPException(status_code=404, detail="Admin no encontrado")
-
-    admin = Admin(**db_admin)
-
-    return admin
